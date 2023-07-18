a6a760fdb58c2e957a76af96bcbab4ddce0c24c0
ayndiara <ayndiara.miranda.00@gmail.comgit config --global user.email ayndiara.miranda.00@gmail.ccom>
2023-07-18 11:30:48 -0300

    Atualização do arquivo Router.jsx

--- ayndiara/projeto-guiado-react/src/Router.jsx
+++ ayndiara/projeto-guiado-react/src/Router.jsx
@@ -3,7 +3,7 @@ import Menu from './components/Menu'
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
 import Portifolio from './pages/Portifolio'
-import Contato from './pages/contato'
+import Contato from './pages/Contato'
 import Footer from './components/Footer'
 
 
